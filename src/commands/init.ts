5e50509accf2ca732fc03bc0cedaa4da1f74d122
Beck Wangthumboon <becktsm@gmail.com>
2025-11-25 22:57:30 -0600

    initial yargs + init command

---
+++ src/commands/init.ts
@@ -0,0 +1,5 @@
+async function init() {
+  console.log('Initializing a new project and setup the project structure');
+}
+
+export { init };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 #!/usr/bin/env bun
-import { main } from './main.js';
 
-main();
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { init } from './commands/init.js';
+
+yargs(hideBin(process.argv))
+  .scriptName('bluprint')
+  .usage('$0 <command> [options]')
+  .command(
+    'init',
+    'Initialize a new project',
+    () => {},
+    () => {
+      init();
+    }
+  )
+  .strict()
+  .demandCommand(1)
+  .version('0.0.1')
+  .alias('h', 'help')
+  .alias('v', 'version').argv;
