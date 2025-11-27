39c46f247e75bac2e0d8cc5600427ebdc1901c38
Beck Wangthumboon <becktsm@gmail.com>
2025-11-27 12:46:29 -0600

    node compatibility

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,9 @@ yargs(hideBin(process.argv))
         () => {},
         (error) => {
           exitWithError(error.message);
-        }
+        },
       );
-    }
+    },
   )
   .strict()
   .demandCommand(1)
