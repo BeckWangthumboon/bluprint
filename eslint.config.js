6b51bc3f33d6ccf9420143bdc4a9596a694bbe74
Beck Wangthumboon <becktsm@gmail.com>
2026-01-10 16:01:14 +0700

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const tsFiles = ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'];
+
+export default [
+  { ignores: ['node_modules', 'dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    files: tsFiles,
+  })),
+  {
+    files: tsFiles,
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+];
